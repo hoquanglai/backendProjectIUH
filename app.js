d28e34eff7254892449c36dde948d48d323273fb
h0quanglai <hquanglai@gmail.com>
2019-09-11 09:28:13 -0700

    upfate

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb://127.0.0.1:27017/ABC';
+var dev_db_url = 'mongodb://127.0.0.1:27017/temp';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 //mongoose.Promise = global.Promise;
